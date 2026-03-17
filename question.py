886da66bbbe0ffdacc3999c70c32e218b5b0db27
JSmeetsCalco <julia.smeets@calco.nl>
2026-03-17 11:38:09 +0100

    Fixed more comments

--- question.py
+++ question.py
@@ -10,12 +10,12 @@ class Question:
         self.review = False
     
     def reset(self):
+        # Reset question to original state
         self.attempts = 0
         self.review = False
     
     def is_correct(self, user):
         # Return True if user answer is close enough to correct answer.
-
         user = user.strip().lower()
         answer = self.answer.strip().lower()
 
@@ -25,6 +25,7 @@ class Question:
         return ratio >= 0.9
             
     def mask_answer(self, user):
+        # Compares user answer with actual answer in typing mode. Displays correct letters and displays '.' or ' ' for incorrect letters
         result = []
 
         for real, typed in zip(self.answer, user):
@@ -35,34 +36,48 @@ class Question:
             else:
                 result.append(".")
         
+        # If user answer is shorter than actual answer, add '.' for any remaining letters
         result += ["." for _ in range(len(self.answer) - len(user))]
 
+        # Return the result answer
         return "".join(result)
     
     def force_copy(self):
+        # After three tries, user is forced to copy full answer
+        # Display correct answer
         print("\nThe correct answer is: ", self.answer)
 
+        # Give user opportunity to copy correct answer, but do not check whether it is correct
         input("Type the answer to continue (or press Enter to skip): ")
 
+        # Add question to review if it is copied
         self.review = True
     
     def show_hint(self):
+        # When user presses Enter, show first letter of each word as hint
         words = self.answer.split()
 
         hints = []
 
         for word in words:
+            # For first hint, show the first letter of each word
             if self.attempts == 0:
                 hints.append(word[0] + "." * (len(word) - 1))
+            # For second hint, show the first two letters of each word
             elif self.attempts == 1:
                 hints.append(word[:2] + "." * (len(word) - 2))
 
+        # Print hint to user
         print("Hint: ", " ".join(hints))
     
     def check_command(self, user, quiz):
+        # Checks whether user typed in a command. All commands start with '/'
+
+        # Return if user input is not a command
         if not user.startswith("/"):
             return False
         
+        # Reset command: Resets round with current questions
         if user == "/reset":
             print(Fore.RED + "\nResetting current round...")
             print(Style.RESET_ALL)
@@ -70,24 +85,29 @@ class Question:
                 q.reset()
             return True
         
+        # Mode command: Switches between typing and flashcard mode
         elif user == "/mode":
             old_mode = quiz.mode
-            quiz.mode = "flashcard" if quiz.mode == "typing" else "typing"
+            quiz.mode = "f" if quiz.mode == "t" else "t"
             print(Fore.RED + f"\nSwitched mode from {old_mode} -> {quiz.mode}")
             print(Style.RESET_ALL)
             return True
         
+        # Remaining command: Shows how many questions remain from the total number of questions in the bank
         elif user == "/remaining":
             total = len(quiz.bank.all_questions)
             left = len(quiz.bank.unused_questions)
             print(Fore.RED + f"\nQuestions remaining in bank: {left} / {total}")
             print(Style.RESET_ALL)
             return True
+
+        # Help command: shows all commands
         elif user == "/help":
             print(Fore.RED + "Commands currently in use:")
             print("/reset   /mode   /remaining  /progress")
             print(Style.RESET_ALL)
 
+        # Progress command: shows how many questions have been asked so far, how many were correct and user accuracy
         elif user == "/progress":
             print(Fore.RED + f"Questions asked so far: {quiz.total_answered}")
             print(f"Correct answers: {quiz.total_correct}")
--- question_bank.py
+++ question_bank.py
@@ -5,6 +5,7 @@ class QuestionBank:
     def __init__(self, topic):
         topic_file = topic.lower().replace(" ", "_") if topic else "network_security"
 
+        # Store all questions and answers from csv file. All_questions is only used to reset unused_questions 
         self.all_questions = []
         self.unused_questions = []
 
@@ -13,24 +14,31 @@ class QuestionBank:
             reader = csv.reader(file, delimiter=";")
 
             for row in reader:
-
+                # Continue of answer or question is missing
                 if len(row) < 2:
                     continue
 
                 q, a = row[0].strip(), row[1].strip()
 
+                # Store questions and answer in all_questions list
                 if q and a:
                     self.all_questions.append((q, a))
         
+        # Copy all_questions, so that a full copy of csv content always exists for reset purposes
         self.unused_questions = self.all_questions.copy()
     
     def get_questions(self, amount):
+        # Load questions based on max_questions user input
+        # Choose either user input for max_questions or the number of questions left if that is less than user input
         amount = min(amount, len(self.unused_questions))
 
+        # Get a number of random questions, equal to max_questions
         selected = random.sample(self.unused_questions, amount)
 
+        # Remove questions from unused_questions
         for q in selected:
             self.unused_questions.remove(q)
         
+        # Return question + answer pairs
         return selected
     
